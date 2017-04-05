a5e94fc2369135d86248fffc2d87866d366be31f
Valentin Giboulot <valentin.giboulot@gmail.com>
2017-04-05 13:39:27 +0200

    premier commit

---
+++ js/pizzeria.js
@@ -0,0 +1,198 @@
+let listeRecipe = [
+{
+	name : 'Regina',
+	toppings :['fromage','jambon']
+
+},
+{
+	name : '3 fromages',
+	toppings :['fromage','fromage']
+
+}
+]
+
+let p1 = {
+	recipe : 'Regina',
+	toppings : []
+}
+
+let p2 = {
+	recipe : 'Regina',
+	toppings : []
+}
+
+
+let l1 = {
+	name : 'toto',
+	toppings :['ananas','fromage']
+
+}
+
+
+
+// TOOLS 
+
+let getStatus = function (p1) {
+
+	let recette = listeRecipe.find(recipe => p1.recipe === recipe.name);
+	
+	let {ok,ko} = recette.toppings.reduce( (acc,item,idx,arr) => {
+
+		let first = p1.toppings.indexOf(item);
+		let last = p1.toppings.lastIndexOf(item)
+
+		if( first !== -1 && first === last) acc.ok++;
+		else acc.ko++;
+		return acc;
+
+	},{ok : 0, ko : 0});
+
+	if (ok < p1.toppings.length) return 'wrong';
+	else if (ko > 0) return 'incomplete';
+	else return 'complete';
+
+}
+
+let findIncomplete = function (listePizza){
+
+	let nbIncomplete = 0;
+
+	listePizza.forEach((val) => {
+
+		if (getStatus(val) === 'incomplete')
+			nbIncomplete++;
+
+	})
+	return nbIncomplete;
+}
+
+let findWrong = function (listePizza){
+
+	let nbIncomplete = 0;
+
+	listePizza.forEach((val) => {
+
+		if (getStatus(val) === 'wrong')
+			nbIncomplete++;
+
+	})
+	return nbIncomplete;
+}
+
+let findComplete = function (listePizza){
+
+	let nbIncomplete = 0;
+
+	listePizza.forEach((val) => {
+
+		if (getStatus(val) === 'complete')
+			nbIncomplete++;
+
+	})
+	return nbIncomplete;
+}
+
+
+
+let loadRecipe = function (listRecipe){
+	listeRecipe.forEach( recipe => {
+
+		let li = document.createElement('li');
+
+		li.innerHTML = recipe.name;
+		li.classList.add('list-group-item');
+
+		let ul = document.getElementById('recipes');;
+		ul.appendChild(li);
+
+	});
+
+
+
+}
+
+
+let addRecipe = function (recipe){
+
+	let h = document.createElement.bind(document);
+
+	listeRecipe.push(recipe);
+
+	let ul = document.getElementById('recipes');;
+	
+	let li = h('li');
+
+	li.innerHTML = recipe.name;
+	li.classList.add('list-group-item');
+
+
+	ul.appendChild(li);
+
+
+}
+
+
+
+loadRecipe(listeRecipe)
+
+
+addRecipe(l1);
+
+
+let button = document.getElementById('buttonAddRecipe');
+
+function getRecipeName() {
+
+	let recipe = {
+		name : document.getElementById('recipeName').value,
+		toppings :[]
+
+	}
+
+	addRecipe(recipe);
+}
+
+button.addEventListener('click', getRecipeName, false);
+
+
+
+let ul = document.getElementById('recipes');
+
+function removeRecipe() {
+
+	let recipe = {
+		name : document.getElementById('recipeName').value,
+		toppings :[]
+
+	}
+
+	addRecipe(recipe);
+}
+
+ul.addEventListener('click', removeRecipe, false);
+
+
+
+
+
+
+let listePiz = [p1,p2];
+
+console.log(findIncomplete(listePiz));
+
+
+
+console.log([listeRecipe]);
+
+
+console.log(getStatus(p1)); //
+p1.toppings.push('ananas');
+console.log(getStatus(p1)); //
+
+
+p2.toppings.push('fromage');
+p2.toppings.push('jambon');
+console.log(getStatus(p2)); //
+
+
+
